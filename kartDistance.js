322522ca1e6db349cc5ca7e2cc384f757b817874
Daniel Baumann <daniel1.baumann@student.uni-augsburg.de>
2017-09-18 17:35:08 +0200

    fix indentation

--- kartDistance.js
+++ kartDistance.js
@@ -8,18 +8,18 @@ function calcDistance(x1, y1, x2, y2){
 
 
 function calcPoint(x1, y1, x2, y2, distanceToPoint1){
-	/*
-	finds coordinates on a line in a cartesian coordinate system 
-	*/
-	var totalDistance = calcDistance(x1, y1, x2, y2);
-	var x_new = (1 - (distance / totalDistance)) * x1 + ((distance / totalDistance) * x2);
-	var y_new = (1 - (distance / totalDistance)) * y1 + ((distance / totalDistance) * y2);
-	/*
-	Exception if distance == 0
-	*/
-	if (velocity === 0){
-		x_new = x1;
-		y_new = y1;
-	}
-	return [x_new, y_new]
+  /*
+  finds coordinates on a line in a cartesian coordinate system
+  */
+  var totalDistance = calcDistance(x1, y1, x2, y2);
+  var x_new = (1 - (distance / totalDistance)) * x1 + ((distance / totalDistance) * x2);
+  var y_new = (1 - (distance / totalDistance)) * y1 + ((distance / totalDistance) * y2);
+  /*
+  Exception if distance == 0
+  */
+  if (velocity === 0){
+  	x_new = x1;
+  	y_new = y1;
+  }
+  return [x_new, y_new]
 }
