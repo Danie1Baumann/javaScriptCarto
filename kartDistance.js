3f6dfd5e57d2c4dae7d043ba5977621f88c91cd8
Daniel Baumann <daniel1.baumann@student.uni-augsburg.de>
2017-09-20 16:52:00 +0200

    Arrays as arguments

--- kartDistance.js
+++ kartDistance.js
@@ -1,25 +1,38 @@
-function calcDistance(x1, y1, x2, y2){
+function calcDistance(xx1, xx2){
   /*
-  calculates Distance between two Points in a cartesian coordinate system
+  - calculates Distance between two Points in a cartesian coordinate system
+  - expects two arrays of the same length [x, y, z, k, ...], [x, y, z, k, ...]
+  - returns distance as float
   */
-  var distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-  return distance;
-}
-
-
-function calcPoint(x1, y1, x2, y2, distanceToPoint1){
-  /*
-  finds coordinates on a line in a cartesian coordinate system
-  */
-  var totalDistance = calcDistance(x1, y1, x2, y2);
-  var x_new = (1 - (distance / totalDistance)) * x1 + ((distance / totalDistance) * x2);
-  var y_new = (1 - (distance / totalDistance)) * y1 + ((distance / totalDistance) * y2);
-  /*
-  Exception if distance == 0
-  */
-  if (velocity === 0){
-  	x_new = x1;
-  	y_new = y1;
+  if (xx1.length !== xx2.length){
+    throw "Invalid Arguments";
+    return;
   }
-  return [x_new, y_new]
+  var distance = 0;
+  for(i = 0; i < xx1.length; i++){
+    distance += Math.pow(xx2[i] - xx1[i], 2);
+  }
+  return parseFloat(Math.sqrt(distance));
 }
+
+
+function calcPoint2D(xy1, xy2, distanceTo_xy1){
+  /*
+  finds coordinates of a point on a line in a 2D cartesian coordinate system
+  return Point as array [x, y]
+  */
+  var totalDistance = calcDistance(xy1, xy2);
+  var x_new = (1 - (distanceTo_xy1 / totalDistance)) * xy1[0] + ((distanceTo_xy1 / totalDistance) * xy2[0]);
+  var y_new = (1 - (distanceTo_xy1 / totalDistance)) * xy1[1] + ((distanceTo_xy1 / totalDistance) * xy2[1]);
+  /*
+  Exception if totalDistance === 0
+  */
+  if (totalDistance === 0){
+  	x_new = xy1[0];
+  	y_new = xy1[1];
+  }
+  return [x_new, y_new];
+}
+
+console.log(calcPoint2D([1,1], [1,5], 2));
+
